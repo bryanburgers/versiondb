a638788e53e778efd7f625f50dbc22388d3c8563
Bryan Burgers <bryan.burgers@gmail.com>
2013-02-17 16:15:18 -0600

    Fix a bug with loading the schema json file

--- index.js
+++ index.js
@@ -6,206 +6,219 @@ var path = require('path');
 var pg = require('pg');
 var semver = require('semver');
 
-function upgrade(databaseUrl, schemaFile, callback) {
-
-	// Load config file
-	var conf = require(schemaFile);
-	var rootDir = path.dirname(schemaFile);
-
-
-	function createVersionTable(connection, next) {
-		console.log("Checking for 'ver.version' table...");
-		connection.query("SELECT * FROM information_schema.tables WHERE table_schema = 'ver' AND table_name = 'version'", function(err, result) {
-			if (err) {
-				next(err);
-			}
-
-			if (result.rowCount === 0) {
-				console.log("'ver.version' table does not exist. Creating...");
-				connection.query("CREATE SCHEMA ver;", function(err, result) {
-					if (err) {
-						next(err);
-						return;
-					}
-
-					connection.query("CREATE TABLE IF NOT EXISTS ver.version ( product varchar(50) PRIMARY KEY, version varchar(8) );", function(err, result) {
-						console.log("ver.version created.");
-						next(err);
-					});
-				});
-			}
-			else {
-				console.log("ver.version table exists.");
-				next(err);
-			}
-		});
-	}
-
-	function getSchemaVersion(connection, product, next) {
-		console.log("Getting product version for product '" + product + "'...");
-		connection.query("SELECT version FROM ver.version WHERE product = $1", [product], function(err, result) {
-			if (err) {
-				next(err);
-				return;
-			}
-
-			if (result.rowCount === 0) {
-				console.log("No product version found. Product must be created.");
-				next(null, null);
-			}
-			else {
-				var ver = result.rows[0]['version'];
-
-				if (!semver.valid(ver)) {
-					console.log("Found database version '" + ver + "' is invalid.");
-					next(new Error("Found database version '" + ver + "' is invalid."));
-				}
-
-				ver = semver.clean(ver);
-
-				console.log("Database version: " + product + "@" + ver);
-				next(null, ver);
-			}
-		});
-	}
-
-	function runUpdate(connection, product, version, filename, create, next) {
-		fs.readFile(path.resolve(rootDir, filename), function(err, data) {
-			if (err) {
-				next(err);
-				return;
-			}
-
-			var query = data.toString('utf-8');
-			var queries = query.split(/;\s*\n/);
-
-			connection.query("BEGIN", function(err, result) {
-				if (err) { next(err); return; }
-
-				async.forEachSeries(queries, function(query, next) {
-					if (query.indexOf('\uFEFF') === 0) {
-						query = query.substring(1);
-					}
-					connection.query(query, next);
-				}, function(err) {
-					if (err) {
-						console.log(err);
-						console.log("Rolling back");
-						connection.query("ROLLBACK", function() {
-							return next(err);
-						});
-					}
-					else {
-						var schemaQuery = "UPDATE ver.version SET version = $1 WHERE product = $2";
-						if (create) {
-							schemaQuery = "INSERT INTO ver.version (product, version) VALUES ($2, $1)";
-						}
-						connection.query(schemaQuery, [version, product], function(err, result) {
-							connection.query("COMMIT", function(err) {
-								if (err) {
-									console.log(err);
-									return next(err);
-								}
-
-								console.log('Committed ' + product + '@' + version);
-								return next();
-							});
-						});
-					}
-				});
-			});
-		});
-	}
-
-	// Connect to the database
-	console.log("Connecting to the database...");
-	pg.connect(databaseUrl, function(err, connection) {
+function createVersionTable(connection, next) {
+	console.log("Checking for 'ver.version' table...");
+	connection.query("SELECT * FROM information_schema.tables WHERE table_schema = 'ver' AND table_name = 'version'", function(err, result) {
 		if (err) {
-			console.log("Failed to connect: " + err);
-			return;
+			next(err);
 		}
 
-		console.log("Connected.");
-		console.log("");
-
-		var schemas = Object.keys(conf);
-
-		createVersionTable(connection, function(err) {
-			if (err) { console.log("ERROR: " + err); return; }
-
-			async.forEachSeries(schemas, function(product, next) {
-				console.log("");
-				console.log("*");
-				console.log("* " + product);
-				console.log("*");
-				var versions = Object.keys(conf[product]);
-				var invalidVersion = false;
-				for (var i = 0; i < versions.length; i++) {
-					if (!semver.valid(versions[i])) {
-						console.log("Invalid version " + versions[i]);
-						invalidVersion = true;
-					}
-					else {
-						versions[i] = semver.clean(versions[i]);
-					}
-				}
-
-				if (invalidVersion) {
-					next(new Error("Invalid versions were found."));
+		if (result.rowCount === 0) {
+			console.log("'ver.version' table does not exist. Creating...");
+			connection.query("CREATE SCHEMA ver;", function(err, result) {
+				if (err) {
+					next(err);
 					return;
 				}
 
-				versions.sort(semver.compare);
+				connection.query("CREATE TABLE IF NOT EXISTS ver.version ( product varchar(50) PRIMARY KEY, version varchar(8) );", function(err, result) {
+					console.log("ver.version created.");
+					next(err);
+				});
+			});
+		}
+		else {
+			console.log("ver.version table exists.");
+			next(err);
+		}
+	});
+}
 
-				console.log("Found versions: " + versions);
+function getSchemaVersion(connection, product, next) {
+	console.log("Getting product version for product '" + product + "'...");
+	connection.query("SELECT version FROM ver.version WHERE product = $1", [product], function(err, result) {
+		if (err) {
+			next(err);
+			return;
+		}
 
-				getSchemaVersion(connection, product, function(err, version) {
-					if (err) { console.log("ERROR: " + err); next(err); return; }
+		if (result.rowCount === 0) {
+			console.log("No product version found. Product must be created.");
+			next(null, null);
+		}
+		else {
+			var ver = result.rows[0]['version'];
 
-					var startIndex = 0;
-					if (version == null) {
-						// Schema doesn't exist, so we need to run all of the versions.
-						console.log("Schema doesn't exist; running all version scripts.");
-						startIndex = 0;
+			if (!semver.valid(ver)) {
+				console.log("Found database version '" + ver + "' is invalid.");
+				next(new Error("Found database version '" + ver + "' is invalid."));
+			}
+
+			ver = semver.clean(ver);
+
+			console.log("Database version: " + product + "@" + ver);
+			next(null, ver);
+		}
+	});
+}
+
+function runUpdate(connection, product, version, filename, create, next) {
+	fs.readFile(path.resolve(rootDir, filename), function(err, data) {
+		if (err) {
+			next(err);
+			return;
+		}
+
+		var query = data.toString('utf-8');
+		var queries = query.split(/;\s*\n/);
+
+		connection.query("BEGIN", function(err, result) {
+			if (err) { next(err); return; }
+
+			async.forEachSeries(queries, function(query, next) {
+				if (query.indexOf('\uFEFF') === 0) {
+					query = query.substring(1);
+				}
+				connection.query(query, next);
+			}, function(err) {
+				if (err) {
+					console.log(err);
+					console.log("Rolling back");
+					connection.query("ROLLBACK", function() {
+						return next(err);
+					});
+				}
+				else {
+					var schemaQuery = "UPDATE ver.version SET version = $1 WHERE product = $2";
+					if (create) {
+						schemaQuery = "INSERT INTO ver.version (product, version) VALUES ($2, $1)";
 					}
-					else {
-						var index = versions.indexOf(version);
-						if (index < 0) {
-							console.log("Version '" + version + "' is not found in version list. Can't continue.");
-							next();
-							return;
-						}
-
-						// We don't need to run the one we're currently on. Go to the next one.
-						startIndex = index + 1;
-					}
-
-					var updateVersions = versions.slice(startIndex);
-					if (updateVersions.length > 0) {
-						console.log("Running updates for versions: " + updateVersions);
-
-						// We can't do an upsert on ver.version, so we need to know whether
-						// we're going to INSERT or UPDATE.
-						var create = startIndex === 0;
-
-						async.forEachSeries(updateVersions, function (version, next) {
-							runUpdate(connection, product, version, conf[product][version], create, next);
-							create = false;
-						}, function(err) {
+					connection.query(schemaQuery, [version, product], function(err, result) {
+						connection.query("COMMIT", function(err) {
 							if (err) {
 								console.log(err);
+								return next(err);
 							}
 
-							next();
+							console.log('Committed ' + product + '@' + version);
+							return next();
 						});
+					});
+				}
+			});
+		});
+	});
+}
+
+function upgrade(databaseUrl, schemaFile, callback) {
+
+	// Load config file
+	fs.readFile(schemaFile, 'utf-8', function(err, jsonData) {
+		if (err) {
+			console.log("Couldn't read file '" + schemaFile + "'.");
+			return callback(err);
+		}
+
+		try {
+			var conf = JSON.parse(jsonData);
+		}
+		catch (err) {
+			console.log("File '" + schemaFile + '" is not valid JSON.');
+			return callback(err);
+		}
+
+		var rootDir = path.dirname(schemaFile);
+
+		// Connect to the database
+		console.log("Connecting to the database...");
+		pg.connect(databaseUrl, function(err, connection) {
+			if (err) {
+				console.log("Failed to connect: " + err);
+				return;
+			}
+
+			console.log("Connected.");
+			console.log("");
+
+			var schemas = Object.keys(conf);
+
+			createVersionTable(connection, function(err) {
+				if (err) { console.log("ERROR: " + err); return; }
+
+				async.forEachSeries(schemas, function(product, next) {
+					console.log("");
+					console.log("*");
+					console.log("* " + product);
+					console.log("*");
+					var versions = Object.keys(conf[product]);
+					var invalidVersion = false;
+					for (var i = 0; i < versions.length; i++) {
+						if (!semver.valid(versions[i])) {
+							console.log("Invalid version " + versions[i]);
+							invalidVersion = true;
+						}
+						else {
+							versions[i] = semver.clean(versions[i]);
+						}
 					}
-					else {
-						console.log("Product '" + product + "' is up to date.");
-						next();
+
+					if (invalidVersion) {
+						next(new Error("Invalid versions were found."));
+						return;
 					}
+
+					versions.sort(semver.compare);
+
+					console.log("Found versions: " + versions);
+
+					getSchemaVersion(connection, product, function(err, version) {
+						if (err) { console.log("ERROR: " + err); next(err); return; }
+
+						var startIndex = 0;
+						if (version == null) {
+							// Schema doesn't exist, so we need to run all of the versions.
+							console.log("Schema doesn't exist; running all version scripts.");
+							startIndex = 0;
+						}
+						else {
+							var index = versions.indexOf(version);
+							if (index < 0) {
+								console.log("Version '" + version + "' is not found in version list. Can't continue.");
+								next();
+								return;
+							}
+
+							// We don't need to run the one we're currently on. Go to the next one.
+							startIndex = index + 1;
+						}
+
+						var updateVersions = versions.slice(startIndex);
+						if (updateVersions.length > 0) {
+							console.log("Running updates for versions: " + updateVersions);
+
+							// We can't do an upsert on ver.version, so we need to know whether
+							// we're going to INSERT or UPDATE.
+							var create = startIndex === 0;
+
+							async.forEachSeries(updateVersions, function (version, next) {
+								runUpdate(connection, product, version, conf[product][version], create, next);
+								create = false;
+							}, function(err) {
+								if (err) {
+									console.log(err);
+								}
+
+								next();
+							});
+						}
+						else {
+							console.log("Product '" + product + "' is up to date.");
+							next();
+						}
+					});
+				}, function(err) {
+					callback(err);
 				});
-			}, function(err) {
-				callback(err);
 			});
 		});
 	});
